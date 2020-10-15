03252abf9a1767cf8023c94edda9bafc73bff433
Augie Menos <augie.menos@gmail.com>
2020-10-15 21:46:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
-def reverse_each_word (string)
-  string_array = [string.split]
-  reversed_array = ""
-  string_array.each do |word|
-    word = ""
-    reversed_array << word.reverse
-  end
-  reversed_array
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
